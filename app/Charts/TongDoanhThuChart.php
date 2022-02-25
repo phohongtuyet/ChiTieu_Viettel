d545ad6daef62f277b2df89cfc0a3d4a18bc3dc8
phohongtuyet <phohongtuyetse@gmail.com>
2022-02-25 10:56:57 +0700

    them chart doanh thu va tong doanh thu

---
+++ app/Charts/TongDoanhThuChart.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Charts;
+
+use ConsoleTVs\Charts\BaseChart;
+use Illuminate\Http\Request;
+use Chartisan\PHP\Chartisan;
+use App\Models\ChiTieu;
+use App\Models\ThucHien;
+class TongDoanhThuChart extends BaseChart
+{
+    /**
+     * Handles the HTTP request for the given chart.
+     * It must always return an instance of Chartisan
+     * and never a string or an array.
+     */
+    public function handler(Request $request): Chartisan
+    {
+        $KH = ChiTieu::select('kenhtruyen','tytrongkenhtruyen')->first();
+        $TH = ThucHien::select('kenhtruyen')->first();
+
+        $ptTH = $TH->kenhtruyen/$KH->kenhtruyen ;
+        $diem = 0 ;
+
+        if($ptTH < 120 )
+        {
+            $diem = $ptTH * $KH->tytrongkenhtruyen;
+        }
+        else
+        {
+            $diem = (120/100) * $KH->tytrongkenhtruyen;
+        }
+
+        return Chartisan::build()
+            ->labels(['Tổng doanh thu  '])
+            ->dataset('Kế hoạch', [$KH->kenhtruyen])
+            ->dataset('Thực hiện', [$TH->kenhtruyen])
+            ->dataset('Phần trâm thực hiện', [$ptTH *100])
+            ->dataset('Điểm ', [number_format($diem,1)]);
+    }
+}
